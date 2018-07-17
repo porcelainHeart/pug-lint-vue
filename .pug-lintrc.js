f116b3569e062a04119b8421e51fa27371fa98c8
wujinze <wujinze@hellobike.com>
2018-07-17 16:50:45 +0800

    feature: 内置配置文件

---
+++ .pug-lintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  disallowDuplicateAttributes: true,
+  disallowClassAttributeWithStaticValue: true,
+  disallowClassLiteralsBeforeIdLiterals: true,
+  disallowHtmlText: true,
+  disallowMultipleLineBreaks: true,
+  disallowSpaceAfterCodeOperator: true,
+  disallowSpacesInsideAttributeBrackets: true,
+  disallowTrailingSpaces: true,
+  requireClassLiteralsBeforeAttributes: true,
+  requireIdLiteralsBeforeAttributes: true,
+  requireLowerCaseTags: true,
+  validateAttributeSeparator: {
+    "separator": " ",
+    "multiLineSeparator": "\n "
+  },
+  validateDivTags: true
+}
